eba9b647fd6ca1d0f3301f18b284958a8d713d22
firebingo <matthew.t.kides@gmail.com>
2022-07-09 22:35:53 -0700

    Convert ffmpeg read process to be triple buffered instead of double. This removes the need for some lock sleep waits and can keep ffmpeg reading more consistently. Update packages.

--- HueScreenAmbience/FFMpegCapture/FFMpegCapture.cs
+++ HueScreenAmbience/FFMpegCapture/FFMpegCapture.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HueScreenAmbience.FFMpegCapture
 {
-	public class FFMpegCapture : IDisposable
+	unsafe public class FFMpegCapture : IDisposable
 	{
 		private readonly int _width = 0;
 		private readonly int _height = 0;
@@ -26,9 +26,10 @@ namespace HueScreenAmbience.FFMpegCapture
 		private Stream _ffmpegStdErrorStream;
 		private bool _isRunning = false;
 		private readonly int _frameLength = 0;
-		private readonly byte[] _frameBuffer;
-		private readonly byte[] _frameBackBuffer;
-		private bool _readLock;
+		private byte[] _presentBuffer;
+		private byte[] _readyBuffer;
+		private byte[] _progressBuffer;
+		private int _readLock;
 
 		private Thread _ffmpegThread;
 		private Thread _ffmpegStdErrorThread;
@@ -52,12 +53,20 @@ namespace HueScreenAmbience.FFMpegCapture
 			_inputFrameRate = inputFrameRate;
 			_bufferMultiplier = bufferMultiplier;
 			_frameLength = _width * _height * 4;
-			_frameBuffer = new byte[_frameLength];
-			_frameBackBuffer = new byte[_frameLength];
-			_readLock = false;
+			_presentBuffer = new byte[_frameLength];
+			_readyBuffer = new byte[_frameLength];
+			_progressBuffer = new byte[_frameLength];
+			_readLock = 1;
 		}
 
 		//gpu_mem=256 /boot/config.txt
+		//The read/write loop for this is triple buffered.
+		// The ffmpeg read process writes and exchanges between _readyBuffer and _progressBuffer.
+		// The read frame process reads and exchanges between _presentBuffer and _readyBuffer.
+		// _readlock is a int being treated as a bool and controlled through Interlocked.
+		// When ffmpeg finishes reading a frame it sets it to 0.
+		// When a frame is requested it reads _readLock and sets it to 1. If the value it read was 0 it means
+		//  it is safe to read the frame from the current buffer then swap again.
 		public void Start()
 		{
 			try
@@ -97,22 +106,39 @@ namespace HueScreenAmbience.FFMpegCapture
 
 		public void ReadLoop()
 		{
-			unsafe
+			var rowSize = _width * 4;
+			var buffer = new byte[rowSize];
+			var frameBytesRead = 0;
+			var bytesRead = 0;
+			do
 			{
-				var rowSize = _width * 4;
-				var buffer = new byte[rowSize];
-				var frameBytesRead = 0;
-				var bytesRead = 0;
-				fixed (byte* framePtr = &_frameBuffer[0])
+				fixed (byte* framePtr = &_progressBuffer[0])
 				{
-					do
+					if (frameBytesRead + rowSize > _frameLength)
 					{
-						if (frameBytesRead + rowSize > _frameLength)
-						{
-							if (!_isRunning)
-								break;
+						if (!_isRunning)
+							break;
 
-							bytesRead = _ffmpegStream.Read(buffer, 0, _frameLength - frameBytesRead);
+						bytesRead = _ffmpegStream.Read(buffer, 0, _frameLength - frameBytesRead);
+						fixed (byte* bufferPtr = &buffer[0])
+						{
+							var j = frameBytesRead;
+							for (var i = 0; i < bytesRead; ++i)
+							{
+								framePtr[j++] = bufferPtr[i];
+							}
+						}
+
+						Buffer.BlockCopy(_progressBuffer, 0, _readyBuffer, 0, _frameLength);
+						Interlocked.Exchange(ref _progressBuffer, _readyBuffer);
+						frameBytesRead = 0;
+						Interlocked.Exchange(ref _readLock, 0);
+					}
+					else
+					{
+						bytesRead = _ffmpegStream.Read(buffer, 0, buffer.Length);
+						if (bytesRead != 0)
+						{
 							fixed (byte* bufferPtr = &buffer[0])
 							{
 								var j = frameBytesRead;
@@ -121,41 +147,11 @@ namespace HueScreenAmbience.FFMpegCapture
 									framePtr[j++] = bufferPtr[i];
 								}
 							}
-
-							do
-							{
-								Thread.Sleep(0);
-							}
-							while (_readLock);
-							_readLock = true;
-
-							Buffer.BlockCopy(_frameBuffer, 0, _frameBackBuffer, 0, _frameLength);
-
-							_readLock = false;
-							frameBytesRead = 0;
+							frameBytesRead += bytesRead;
 						}
-						else
-						{
-							if (!_isRunning)
-								_readLock = false;
-
-							bytesRead = _ffmpegStream.Read(buffer, 0, buffer.Length);
-							if (bytesRead != 0)
-							{
-								fixed (byte* bufferPtr = &buffer[0])
-								{
-									var j = frameBytesRead;
-									for (var i = 0; i < bytesRead; ++i)
-									{
-										framePtr[j++] = bufferPtr[i];
-									}
-								}
-								frameBytesRead += bytesRead;
-							}
-						}
-					} while (_isRunning);
+					}
 				}
-			}
+			} while (_isRunning);
 		}
 
 		public void ReadStdErrorLoop()
@@ -173,19 +169,14 @@ namespace HueScreenAmbience.FFMpegCapture
 			{
 				var start = DateTime.UtcNow;
 				var frameData = frameStream.GetBuffer();
-				do
+				var t = DateTime.UtcNow;
+				Buffer.BlockCopy(_presentBuffer, 0, frameData, 0, _frameLength);
+				if (Interlocked.Exchange(ref _readLock, 1) == 1)
 				{
-					Thread.Sleep(0);
+					return false;
 				}
-				while (_readLock);
-				_readLock = true;
-				unsafe
-				{
-					var t = DateTime.UtcNow;
-					Buffer.BlockCopy(_frameBackBuffer, 0, frameData, 0, _frameLength);
-					//Console.WriteLine($"Buffer.BlockCopy: {(DateTime.UtcNow - t).TotalMilliseconds}");
-				}
-				_readLock = false;
+				Interlocked.Exchange(ref _readyBuffer, _presentBuffer);
+				//Console.WriteLine($"Buffer.BlockCopy: {(DateTime.UtcNow - t).TotalMilliseconds}");
 				//Console.WriteLine($"GetFrame: {(DateTime.UtcNow - start).TotalMilliseconds}");
 				return true;
 			}
@@ -216,6 +207,7 @@ namespace HueScreenAmbience.FFMpegCapture
 				_ffmpegStdErrorStream?.Dispose();
 			}
 
+			Interlocked.Exchange(ref _readLock, 0);
 			GC.SuppressFinalize(this);
 		}
 	}
